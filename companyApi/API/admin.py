aaf41445c75569c1e7cb27ccbb2d877649b9bd62
yogendradhami <yogendradhami6@gmail.com>
2023-04-20 17:19:07 +0545

    model

--- companyApi/API/admin.py
+++ companyApi/API/admin.py
@@ -4,9 +4,11 @@ from API.models import Company , Employee
 
 class CompanyAdmin(admin.ModelAdmin):
     list_display=('name','location', 'type')
+    search_fields= ('name',)
 
 class EmployeeaAdmin(admin.ModelAdmin):
     list_display= ('name', 'email', 'company')
-
+    list_filter=('company',)
+    
 admin.site.register(Employee, EmployeeaAdmin)
 admin.site.register(Company, CompanyAdmin)
