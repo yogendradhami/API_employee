3575b10c88c253ae068015bfb037bdd7ba9b1603
yogendradhami <yogendradhami6@gmail.com>
2023-04-20 08:11:17 +0545

    rest api packages are installed and initialized

--- companyApi/app_company/views.py
+++ companyApi/app_company/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+def Home(request):
+    return HttpResponse("This is homepage")
